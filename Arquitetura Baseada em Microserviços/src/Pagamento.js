fbc86fd1eaefb5d18428025673622ccf3fa51352
Marcelo Gregório Vieira <marcelovieira8967@gmail.com>
2025-02-01 16:57:05 -0300

    modified:   "Arquitetura Baseada em Microservi\303\247os/src/Catalogo.js" 	modified:   "Arquitetura Baseada em Microservi\303\247os/src/Pagamento.js"

--- Arquitetura Baseada em Microserviços/src/Pagamento.js
+++ Arquitetura Baseada em Microserviços/src/Pagamento.js
@@ -1,7 +1,12 @@
-export default class Pagamento{
+export default class Pagamento {
 
-    constructor(){
-        
+    processarPagamento(pedido) {
+        if (saldo < pedido.total){
+            console.log('Falha no pagamento: Saldo insuficiente')
+        }
+        else {
+            pedido.status = 'pago'
+            console.log('Pagamento do pedido aprovado!')
+        }
     }
-
 }

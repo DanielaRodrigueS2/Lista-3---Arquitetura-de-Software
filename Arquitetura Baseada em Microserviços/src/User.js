ff1cb3a2a4a58f257a8c9b67aee32a5515b19215
Daniela <danielarodrigues2po@gmail.com>
2025-02-01 14:59:07 -0300

    Criação classe user

---
+++ Arquitetura Baseada em Microserviços/src/User.js
@@ -0,0 +1,35 @@
+export default class User{
+    
+    constructor(cpf, nome, email,senha){
+        this.cpf = cpf
+        this.nome = nome
+        this.email = email
+        this.senha = senha
+        this.saldo = 0.0
+    }
+
+    getNome(){
+        return this.nome
+    }
+
+    getCpf(){
+        return this.cpf
+    }
+
+    getEmail(){
+        return this.email
+    }
+
+    getSenha(){
+        return this.senha
+    }
+
+    getSaldo(){
+        return this.saldo
+    }
+
+    alterarSaldo(valor){
+        this.saldo += valor
+    }
+    
+}
